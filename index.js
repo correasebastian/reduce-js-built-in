32027dae80ced01b997ab32d7a7640d21f3deb2f
sebastian correa <scorrea1@wsgc.com>
2018-02-11 14:12:36 -0500

    reduce vs filter and map

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var data = [1, 2, 3, 4, 5, 6]
 
-var onlyEven = data.reduce(
-    (acc, curr) =>
-        curr % 2 === 0
-            ? acc.concat(curr)
-            : acc
-    , []
-)
+var bigData = [];
+for (var i = 0; i < 10000000; i++) {
+  bigData[i] = i;
+}
 
-console.log(onlyEven);
 
+let filterBegin = Date.now()
+var filterMappedBigData = bigData.filter(function(value) {
+  return value % 2 === 0;
+}).map(function(value) {
+  return value * 2;
+});
+let filterEnd = Date.now()
+let filtertimeSpent = (filterEnd-filterBegin)/1000 + "secs";
+
+
+let reducedBegin=Date.now();
+var reducedBigData = bigData.reduce(function(acc, value) {
+  if (value % 2 === 0) {
+    acc.push(value * 2);
+  }
+  return acc;
+
+  /* return value % 2 === 0 
+  ? acc.concat(value*2)
+  : acc */ // this takes much longer 
+}, []);
+let reducedEnd = Date.now();
+let reducedtimeSpent = (reducedEnd-reducedBegin)/1000 + " secs";
+
+console.log("filtered Big Data:", filtertimeSpent)
+console.log("reduced Big Data:", reducedtimeSpent)
